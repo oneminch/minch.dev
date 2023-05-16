3e87b50ce4850e1fecc55ff3952545533d2aa0c3
oneminch <27751688+oneminch@users.noreply.github.com>
2023-05-15 23:51:46 -0500

    Add middleware to fix scroll to top issues when route changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
         lang: "en"
       },
       title: "Dawit",
-      titleTemplate: "",
+      // titleTemplate: "",
       meta: [
         { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
         { charset: "utf-8" },
