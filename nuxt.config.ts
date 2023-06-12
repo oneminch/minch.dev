98f3c949e120d248663f8c337405752bcfd70ffc
oneminch <27751688+oneminch@users.noreply.github.com>
2023-06-11 22:18:24 -0400

    Reset Nitro config for Vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
     "@nuxtjs/partytown"
   ],
   nitro: {
-    preset: "cloudflare-pages"
+    // preset: "cloudflare-pages"
   },
   routeRules: {
     // "/blog/**": { static: true }
