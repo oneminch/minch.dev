3bb0adc25418b0d812fe50dd5f001c4a1324e76a
oneminch <27751688+oneminch@users.noreply.github.com>
2023-09-27 19:32:41 -0400

    Add an accessible theme toggle - Modify size

--- components/AppThemeToggle.vue
+++ components/AppThemeToggle.vue
@@ -7,7 +7,7 @@
 <!-- Theme Toggle -->
 <template>
   <label
-    class="flex h-7 w-10 items-center p-1 cursor-pointer rounded-full bg-zinc-200 dark:bg-zinc-700 border border-zinc-300 dark:border-zinc-600 text-zinc-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-opacity-50 focus-within:ring-offset-0"
+    class="flex h-6 w-10 p-[.125rem] items-center cursor-pointer rounded-full bg-zinc-200 dark:bg-zinc-700 border border-zinc-300 dark:border-zinc-600 text-zinc-700 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-opacity-50 focus-within:ring-offset-0"
     title="Toggle Theme"
   >
     <input
@@ -18,10 +18,11 @@
     />
     <span
       id="theme-icon"
-      class="h-5 w-5 p-1 flex items-center justify-center rounded-full text-center bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100 peer-checked:translate-x-1/2 text-xs shadow cursor-pointer"
+      class="h-5 w-5 p-1 flex items-center justify-center rounded-full text-center bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100 peer-checked:translate-x-3/4 text-xs shadow cursor-pointer"
       @click="toggle()"
     >
-      <Icon :name="`heroicons:${isDark ? 'moon' : 'sun'}-20-solid`" />
+      <Icon v-if="isDark" name="heroicons:moon-20-solid" />
+      <Icon v-else name="heroicons:sun-20-solid" />
     </span>
   </label>
 </template>
