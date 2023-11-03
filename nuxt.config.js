2608c1570b0e0b60644d154e01097c768c4a8ece
oneminch <27751688+oneminch@users.noreply.github.com>
2023-11-02 23:18:59 -0400

    feat: design and add custom multi-platform favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,33 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           href: "https://fonts.bunny.net/css?family=roboto-mono:400,400i,500,600,700|inter:300,400,500,600,700,800"
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png"
+        },
+        {
+          rel: "icon",
+          sizes: "32x32",
+          href: "/favicon-32x32.png"
+        },
+        {
+          rel: "icon",
+          sizes: "16x16",
+          href: "/favicon-16x16.png"
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest"
         }
+        /**
+         * <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+         * 
+         */
       ]
       // ,script: [
       //   {
