67758fe2b99011b71a89e4ef08d5d0b70bae0e1e
oneminch <27751688+oneminch@users.noreply.github.com>
2023-12-28 15:46:20 -0500

    fix: add focus state styles to contact button

--- pages/index.vue
+++ pages/index.vue
@@ -186,14 +186,14 @@
         class="grid grid-cols-2 lg:grid-cols-4 lg:grid-rows-2 lg:grid-flow-col gap-2 lg:[&_:first-child]:row-span-2 lg:[&_:last-child]:row-span-2 [&_:first-child]:col-span-2 [&_:last-child]:col-span-2 md:[&_:first-child]:col-span-1 md:[&_:last-child]:col-span-1"
       >
         <div
-          class="flex items-center justify-center p-[1px] overflow-hidden font-medium shrink-0 relative -z-0 before:content-[''] before:absolute before:-inset-[1px] before:-z-10 before:bg-gradient-to-b before:from-green-500 before:to-lime-400 rounded-[calc(.375rem+1px)] dark:before:opacity-60"
+          class="flex items-center justify-center p-[1px] overflow-hidden font-medium shrink-0 relative -z-0 before:content-[''] before:absolute before:-inset-[1px] before:-z-10 before:bg-gradient-to-b before:from-green-500 before:to-lime-400 rounded-[calc(.375rem+1px)] dark:before:opacity-60 focus-within:ring-2 ring-offset-0 focus-within:ring-green-400 focus-within:ring-opacity-75"
         >
           <div
             class="flex flex-col items-center justify-center w-full h-full gap-6 p-4 bg-white rounded-md dark:bg-zinc-800"
           >
             <p class="text-lg font-bold">Let's Chat.</p>
             <nuxt-link
-              class="px-4 py-2 bg-green-500 rounded-lg text-zinc-800"
+              class="px-4 py-2 bg-green-500 rounded-lg focus:outline-none text-zinc-800"
               to="/contact"
               id="contact-btn"
               >Message</nuxt-link
