cac712286511baba3c04ff6f3917b20ef2ff060f
oneminch <27751688+oneminch@users.noreply.github.com>
2024-07-07 19:05:24 -0400

    refactor: make bookmark skeletons consistent with bookmark cards
    - Simplify data fetching for bookmarks page

--- components/skeletons/AppBookmarkSkeleton.vue
+++ components/skeletons/AppBookmarkSkeleton.vue
@@ -1,16 +1,11 @@
 <!-- Bookmark: Skeleton -->
 <template>
-  <div class="flex items-center justify-start w-full px-2 py-2 mb-4">
+  <div class="flex items-center justify-start w-full p-2 mb-2">
     <div
-      class="flex-shrink-0 w-8 h-8 mr-3 overflow-hidden rounded-md md:w-12 md:10 animate-pulse bg-zinc-300 dark:bg-zinc-700"
+      class="flex-shrink-0 w-10 h-8 mr-3 overflow-hidden rounded bg-zinc-300 dark:bg-zinc-700 animate-pulse"
     ></div>
-    <div class="w-full">
-      <div
-        class="w-1/2 h-3 mb-1 rounded-full animate-pulse bg-zinc-300 dark:bg-zinc-700"
-      ></div>
-      <div
-        class="w-1/3 h-3 mb-1 rounded-full animate-pulse bg-zinc-300 dark:bg-zinc-700"
-      ></div>
-    </div>
+    <div
+      class="w-2/5 h-3 mb-1 rounded-full animate-pulse bg-zinc-300 dark:bg-zinc-700"
+    ></div>
   </div>
 </template>
