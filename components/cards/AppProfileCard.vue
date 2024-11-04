74916b9a713dbd39acce0c68ac4386d8e83ff4e9
oneminch <27751688+oneminch@users.noreply.github.com>
2024-11-04 10:41:45 -0500

    refactor: include link to Bluesky profile

--- components/cards/AppProfileCard.vue
+++ components/cards/AppProfileCard.vue
@@ -1,6 +1,7 @@
 <script setup>
   const profiles = {
     GitHub: "https://github.com/oneminch",
+    Bluesky: "https://bsky.app/profile/minch.dev",
     X: "https://twitter.com/oneminch",
     LinkedIn: "https://linkedin.com/in/oneminch"
   };
@@ -9,8 +10,7 @@
 <!-- Profile card: Profile Picture, Name, Title + Social Profiles -->
 <template>
   <div
-    class="w-full flex flex-col items-center bg-transparent relative [&>*]:mb-1 md:[&>*]:mb-3 last:[&>*]:mb-0 p-1 pb-2 md:pb-1"
-  >
+    class="w-full flex flex-col items-center bg-transparent relative [&>*]:mb-1 md:[&>*]:mb-3 last:[&>*]:mb-0 p-1 pb-2 md:pb-1">
     <nuxt-img
       preload
       placeholder
@@ -20,8 +20,7 @@
       height="112"
       src="/profile.jpg"
       alt="Profile Picture"
-      class="text-center border rounded-full w-28 border-zinc-200 dark:border-zinc-800"
-    />
+      class="text-center border rounded-full w-28 border-zinc-200 dark:border-zinc-800" />
     <h2 class="text-xl font-bold md:text-2xl text-zinc-900 dark:text-zinc-100">
       Dawit
     </h2>
@@ -29,8 +28,7 @@
       Full-Stack Engineer
     </h2>
     <div
-      class="flex items-center justify-center w-full h-8 text-zinc-900 dark:text-zinc-200"
-    >
+      class="flex items-center justify-center w-full h-8 text-zinc-900 dark:text-zinc-200">
       <nuxt-link
         v-for="(profileUrl, name) in profiles"
         :key="name"
@@ -39,8 +37,7 @@
         :title="name"
         :aria-label="`Link to My ${name} Profile`"
         target="_blank"
-        external
-      >
+        external>
         <Icon :name="`simple-icons:${name.toLowerCase()}`" />
       </nuxt-link>
     </div>
