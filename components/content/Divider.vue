62d58ee1ae9e00303b7d18b2fe62726b940e7c4a
oneminch <27751688+oneminch@users.noreply.github.com>
2024-11-11 07:58:44 -0500

    feat: create a divider component for content

---
+++ components/content/Divider.vue
@@ -0,0 +1,4 @@
+<template>
+  <hr
+    class="w-2/3 mx-auto my-8 block border-none bg-gradient-to-r from-transparent via-zinc-300 dark:via-zinc-700 to-transparent h-[1px] rounded-full" />
+</template>
