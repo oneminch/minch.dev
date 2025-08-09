37433dc2c43e115e41f622b8f395ea6bac3a374c
oneminch <27751688+oneminch@users.noreply.github.com>
2025-08-09 11:59:22 -0400

    content: update projects listing and related
    Closes #98

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,89 +1,89 @@
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/content',
-    '@nuxtjs/partytown',
-    '@nuxtjs/turnstile',
-    '@nuxt/image',
-    '@vueuse/nuxt',
-    '@nuxt/icon'
+    "@nuxt/content",
+    "@nuxtjs/partytown",
+    "@nuxtjs/turnstile",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    "@nuxt/icon"
   ],
   app: {
-    rootId: 'app',
-    rootTag: 'body',
+    rootId: "app",
+    rootTag: "body",
     head: {
       htmlAttrs: {
-        lang: 'en'
+        lang: "en"
       },
       meta: [
-        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-        { name: 'author', content: 'Dawit (@oneminch)' },
-        { name: 'robots', content: 'index, follow' }
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+        { name: "author", content: "Dawit (@oneminch)" },
+        { name: "robots", content: "index, follow" }
       ],
       link: [
         {
-          rel: 'apple-touch-icon',
-          sizes: '180x180',
-          href: '/apple-touch-icon.png'
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png"
         },
         {
-          rel: 'icon',
-          sizes: '32x32',
-          href: '/favicon-32x32.png'
+          rel: "icon",
+          sizes: "32x32",
+          href: "/favicon-32x32.png"
         },
         {
-          rel: 'icon',
-          sizes: '16x16',
-          href: '/favicon-16x16.png'
+          rel: "icon",
+          sizes: "16x16",
+          href: "/favicon-16x16.png"
         },
         {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/favicon.ico'
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico"
         },
         {
-          rel: 'manifest',
-          href: '/site.webmanifest'
+          rel: "manifest",
+          href: "/site.webmanifest"
         }
       ]
     },
-    pageTransition: { name: 'slide', mode: 'out-in' },
-    layoutTransition: { name: 'slide', mode: 'out-in' }
+    pageTransition: { name: "slide", mode: "out-in" },
+    layoutTransition: { name: "slide", mode: "out-in" }
   },
 
-  compatibilityDate: '2025-05-16',
+  compatibilityDate: "2025-05-16",
 
-  components: [{ path: '~/components', pathPrefix: false }],
+  components: [{ path: "~/components", pathPrefix: false }],
 
   content: {
     build: {
       markdown: {
         toc: { depth: 1, searchDepth: 2 },
         highlight: {
-          theme: 'github-dark',
-          langs: ['js', 'md', 'py']
+          theme: "github-dark",
+          langs: ["js", "md", "py"]
         }
       }
     }
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   devtools: {
     enabled: true
   },
 
   icon: {
-    mode: 'svg',
+    mode: "svg",
     clientBundle: {
       scan: true
     }
   },
 
   routeRules: {
-    '/': { prerender: true },
-    '/**': { prerender: true }
+    "/": { prerender: true },
+    "/**": { prerender: true }
   },
 
   runtimeConfig: {
